97842c8236715d6f54adb2413e870ac5188e4fb6
azizbekarximed03 <85345739+azizbekarximed03@users.noreply.github.com>
2022-05-08 11:31:02 +0500

    Add files via upload

---
+++ LOCAL/localisation.py
@@ -0,0 +1,45 @@
+#Don't be a thief by stealing other's Hardwork, it took time and effort to make this repo.
+#Respect my work by not making any changes here.
+
+START_TEXT = "Video yuboring"
+
+FORCE_SUB_TEXT = "Botni ishlatish uchun kanalga obina bo'ling"
+
+CHANNEL_LINK = "https://t.me/Azizbek_03"
+
+SUPPORT_LINK = "https://t.me/lfraganus"
+
+info_text = "Bot hali chala"   
+
+help_text = """**v1.4**
+
+•`Encode` - encode your video into different lib format or resolution
+
+•`HEVC compress` - negligible loss compression
+
+•`FAST compress` - Very fast and Efficient compression
+
+•`Convert` - change formats or extract audio of any video
+
+•`Rename` - rename any file, extension not required
+
+•`SSHOTS` - generate 10 screenshots of your video
+
+•`Trim` - cut your videos"""
+
+source_text = "**t.me/Azizbek_03**"
+
+DEV = "https://t.me/lfraganus"
+
+spam_notice = "Spamga ban :)"
+
+JPG = "LOCAL/video_convertor.jpg"
+
+JPG0 = 'https://telegra.ph/file/d98c559b56ef884ef3bad.jpg'
+
+JPG2 = "LOCAL/20211215_165751.jpg"
+
+JPG3 = "LOCAL/PicsArt_12-16-08.57.15.jpg"
+
+JPG4 = "LOCAL/20211219_000258.jpg"
+
---
+++ README.md
@@ -0,0 +1,17 @@
+AUTH_USERS COMMANDS:
+  
+  - `/disallow <id>` : to ban
+  - `/allow <id>` : to unban
+  - `/users` : count total users
+  - `/msg <id>` : give this command replying to a message
+  - `/bcast` : Reply to any kind of message to broadcast it
+  - `menu` >> `settings` >>  `restart`
+  
+Deploy your bot on `Okteto`
+  
+[![Develop on Okteto](https://okteto.com/develop-okteto.svg)](https://cloud.okteto.com)
+
+LICENSE:
+
+[![License](https://www.gnu.org/graphics/gplv3-127x51.png)](LICENSE)
+
